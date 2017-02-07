4a1f3ea8e7354717dc8abfe9f4046b6b4879ce27
jpdoria <jp@lazyadm.in>
2017-02-07 19:07:14 +0800

    Code cleanup

--- ipv6zero.py
+++ ipv6zero.py
@@ -8,7 +8,7 @@ def save_file(ok_list, dst_file):
         with open(dst_file, 'a') as fh:
             for elem in ok_list:
                 fh.write('{}\n'.format(elem))
-    except Exception:
+    except:
         raise
 
 
@@ -16,31 +16,31 @@ def process_file(src_file):
     try:
         with open(src_file, 'r') as fh:
             lines = fh.readlines()
-
-        ip_addresses = [(line.strip()) for line in lines]
-        chunks = [(ip.split(':')) for ip in ip_addresses]
-        chunks_len = len(chunks)
-        index = 0
-        ok_list = []
-
-        while index != chunks_len:
-            print('Checking {}...'.format(ip_addresses[index]))
-
-            for elem in chunks[index]:
-                if len(elem) != 4:
-                    new_elem = '{}0'.format(elem)
-                    elem_index = chunks[index].index(elem)
-                    chunks[index][elem_index] = new_elem
-
-                    print('3-char chunk found: {}'.format(elem))
-                    print('NewValue: {}'.format(new_elem))
-
-            ok_list.append(':'.join(chunks[index]))
-            index += 1
-
-        return ok_list
-    except Exception:
+    except:
         raise
+
+    ip_addresses = [(line.strip()) for line in lines]
+    chunks = [(ip.split(':')) for ip in ip_addresses]
+    chunks_len = len(chunks)
+    index = 0
+    ok_list = []
+
+    while index != chunks_len:
+        print('Checking {}...'.format(ip_addresses[index]))
+
+        for elem in chunks[index]:
+            if len(elem) != 4:
+                new_elem = '{}0'.format(elem)
+                elem_index = chunks[index].index(elem)
+                chunks[index][elem_index] = new_elem
+
+                print('3-char chunk found: {}'.format(elem))
+                print('NewValue: {}'.format(new_elem))
+
+        ok_list.append(':'.join(chunks[index]))
+        index += 1
+
+    return ok_list
 
 
 def main():
